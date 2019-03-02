de4b9acf4b7be4bc821d8d10dddc15fcc55e7f7c
sheroshtan <sheroshtan1@gmail.com>
2019-03-02 06:13:30 +0300

    initialized chart and scrollbar. chart added on index page

--- app/js/init.js
+++ app/js/init.js
@@ -38,11 +38,49 @@ $( function() {
 
         e.preventDefault();
         var target = $(this).attr('data-target');
-        $(target).bPopup();
+        $(target).bPopup({
+            closeClass: 'close-modal'
+        });
 
     });
 
     // form element styler init
     $('select').styler();
+
+    // scrollbar
+    $('.scrollbar-inner').scrollbar();
+
+    // chart init
+    var chartBlock = document.getElementById('myChart');
+    if(chartBlock) {
+        var myChart = new Chart(chartBlock, {
+            type: 'bar',
+            data: {
+                labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09'],
+                datasets: [
+                    {
+                        label: 'Signups',
+                        data: [100, 200, 220, 170, 130, 90, 156, 180, 130],
+                        backgroundColor: 'rgba(128, 191, 202, 1)'
+                    },
+                    {
+                        label: 'FTD',
+                        data: [200, 100, 120, 190, 110, 150, 156, 180, 130],
+                        backgroundColor: 'rgba(255, 154, 56, 1)'
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+    }
 
 } );
